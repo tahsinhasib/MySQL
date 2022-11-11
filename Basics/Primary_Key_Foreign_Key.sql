3feeca6fbcfa82547843a83d5e809cd2c3e675a9
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2022-11-11 17:21:33 +0600

    Update Primary_Key_Foreign_Key.sql

--- Basics/Primary_Key_Foreign_Key.sql
+++ Basics/Primary_Key_Foreign_Key.sql
@@ -30,6 +30,20 @@
 */
 ---------------------------------------------------------------------------------
 
+
+---------------------------------------------------------------------------------
+/*
+    The FOREIGN KEY constraint is used to prevent actions that would destroy 
+    links between tables.
+
+    A FOREIGN KEY is a field (or collection of fields) in one table,
+    that refers to the PRIMARY KEY in another table.
+    
+    The table with the foreign key is called the child table, 
+    and the table with the primary key is called the referenced or parent table.
+*/
+---------------------------------------------------------------------------------
+
 CREATE TABLE DEPT(
     DID NUMBER(10) CONSTRAINT DEPT_DID_PK PRIMARY KEY,
     DNAME VARCHAR2(50)
