140539bc6cf8e7817957afa6d05fd71002092061
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2022-11-20 00:58:58 +0600

    Update Adding_Column_Table.sql

--- Basics/Adding_Column_Table.sql
+++ Basics/Adding_Column_Table.sql
@@ -16,20 +16,7 @@ FROM AIUB
 INSERT INTO AIUB VALUES (1, 3500.00, 'DATABASE', 'STEVEN')
 INSERT INTO AIUB VALUES (2, 3500.00, 'DATA STRUCTURE', 'BRANDON')
 
--------------------------------------------------------------------------------
-/*
-    ALTER TABLE <table name> ADD (
-        <col name> <type> (size),
-        <col name> <type> (size),
-        ........................,
-    );
-
-
-    can add as many as we like at one action
-
-*/
--------------------------------------------------------------------------------
-
+----------------------------------------------------------------------
 
 ALTER TABLE AIUB ADD (
    COURSES VARCHAR2 (255),
