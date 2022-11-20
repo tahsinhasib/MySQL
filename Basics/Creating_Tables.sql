4f4c55c4cd01fe1345be88675a527fa9e59d9758
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2022-11-20 10:50:34 +0600

    Update Creating_Tables.sql

--- Basics/Creating_Tables.sql
+++ Basics/Creating_Tables.sql
@@ -9,29 +9,11 @@ DESC AIUB
 
 DROP TABLE AIUB
 
-
-
-
-
 --------------------------------------------------------------------------------
-/*
-   DESC - displays the table details
-   DROP - drops/removes the table
-
-   CREATE TABLE <table name> (<col1> <type> (size),
-                              <col2> <type> (size),
-                              ....................,
-                              ....................,
-   );
-
-
-*/
---------------------------------------------------------------------------------
-
 
 CREATE TABLE AIUB(
-   ID NUMBER(8) CONSTRAINT AIUB_ID_PK PRIMARY KEY, /*id number of students*/
-   CREDIT DECIMAL(4,2),  --floating value
+   ID NUMBER(8) CONSTRAINT AIUB_ID_PK PRIMARY KEY,          --id number of students
+   CREDIT DECIMAL(4,2),                                     --floating value
    COURSE VARCHAR2(20),
    INSTRUCTOR VARCHAR2(30)
 );
