cbbee8995cd943efa376ad109e02e7da2b2e9d2b
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2022-11-20 10:52:18 +0600

    Update Data_Constraints.sql

--- Basics/Data_Constraints.sql
+++ Basics/Data_Constraints.sql
@@ -1,5 +1,4 @@
 
-
 CREATE TABLE STUDENT(
     SID NUMBER(5),
     SNAME VARCHAR2(50)
@@ -11,17 +10,8 @@ INSERT INTO STUDENT(SNAME) VALUES('Alex');      -- will work
 
 DROP TABLE STUDENT
 
-
 ---------------------------------------------------------------------------------
-/*
-    NOT NULL means that particular column value cannot be empty
-
-    ie. not nullable
-
-    <col> <datatype>(size) NOT NULL
-*/
----------------------------------------------------------------------------------
-
+-- NOT NULL
 
 CREATE TABLE STUDENT(
     SID NUMBER(5) NOT NULL,
@@ -40,18 +30,7 @@ DROP TABLE STUDENT
 
 
 ---------------------------------------------------------------------------------
-/*
-    DEFAULT <value> means that particular column value will be the exact value
-    by default that we are going to assign if no value is inserted.
-
-    ie. default
-
-
-    <col_name> <data type> (size) DEFAULT (default_value)
-
-*/
----------------------------------------------------------------------------------
-
+-- DEFAULT <val>
 
 CREATE TABLE STUDENT(
     SID NUMBER(5) DEFAULT 0,
@@ -68,14 +47,7 @@ DROP TABLE STUDENT
 
 
 ---------------------------------------------------------------------------------
-/*
-    UNIQUE means that particular column will take unique value
-    each time a row is going to be inserted.
-
-    <col> <datatype>(size) UNIQUE
-
-*/
----------------------------------------------------------------------------------
+-- UNIQUE
 
 CREATE TABLE STUDENT(
     SID NUMBER(5) UNIQUE,
